fc2602fa3b4bc4c2694aadfced6fe0c3bafa3f72
Jonas Gomes <jonaszd99@gmail.com>
2021-03-27 17:39:34 -0300

    Integrante model

--- app/src/main/java/br.edu.ifpb/model/Integrante.java
+++ app/src/main/java/br.edu.ifpb/model/Integrante.java
@@ -1,4 +1,47 @@
 package br.edu.ifpb.model;
 
 public class Integrante {
+    private int id;
+    private String nome;
+    private LocalDate dataDeNascimento;
+    private String cpf;
+
+    public Integrante(int id, String nome, LocalDate dataDeNascimento, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
