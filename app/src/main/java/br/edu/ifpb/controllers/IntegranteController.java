4c8e31ff8a63f1a13c60aa1f6a4f1ae4bbc84ea5
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2021-04-11 17:51:59 -0300

    Atualizando a listagem

--- app/src/main/java/br/edu/ifpb/controllers/IntegranteController.java
+++ app/src/main/java/br/edu/ifpb/controllers/IntegranteController.java
@@ -1,5 +1,10 @@
 package br.edu.ifpb.controllers;
 
+import br.edu.ifpb.dao.BandaDao;
+import br.edu.ifpb.dao.IntegranteDao;
+import br.edu.ifpb.model.Banda;
+import br.edu.ifpb.model.Integrante;
+
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
@@ -7,4 +12,10 @@ import java.io.Serializable;
 @Named
 @SessionScoped
 public class IntegranteController implements Serializable {
+
+    private Integrante integrante = new Integrante();
+
+    private IntegranteDao integranteDao = new IntegranteDao();
+
+
 }
