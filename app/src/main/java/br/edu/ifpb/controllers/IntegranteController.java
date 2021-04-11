958aef58b51e19daab3db4f5d8a2d9a1d7db0ddb
daniel-dantas <daniel.dantas.ip19@hotmail.com>
2021-04-11 19:56:55 -0300

    resolvendo um conflito

--- app/src/main/java/br/edu/ifpb/controllers/IntegranteController.java
+++ app/src/main/java/br/edu/ifpb/controllers/IntegranteController.java
@@ -30,9 +30,6 @@ public class IntegranteController implements Serializable {
     public void editarIntegrante(){
         integranteDao.editar(integrante);
     }
-    public List<Integrante> listarIntegrantes(){
-        return integranteDao.listar();
-    }
   
     public Integrante getIntegrante() {
         return integrante;
