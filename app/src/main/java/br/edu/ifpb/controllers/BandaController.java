c9d176742bd3d6c9c935bfc3c82854909917b4df
daniel-dantas <daniel.dantas.ip19@hotmail.com>
2021-04-11 12:45:43 -0300

    criando funcionalidade de cadastrar banda

--- app/src/main/java/br/edu/ifpb/controllers/BandaController.java
+++ app/src/main/java/br/edu/ifpb/controllers/BandaController.java
@@ -15,7 +15,7 @@ public class BandaController implements Serializable {
 
     private Banda banda = new Banda();
 
-    private List<Banda> bandas = new ArrayList<Banda>();
+//    private List<Banda> bandas = new ArrayList<Banda>();
 
     private BandaDao bandaDao = new BandaDao();
 
@@ -25,6 +25,14 @@ public class BandaController implements Serializable {
 
     public List<Banda> listarBandas(){
         return bandaDao.listar();
+    }
+
+    public Banda getBanda() {
+        return banda;
+    }
+
+    public void setBanda(Banda banda) {
+        this.banda = banda;
     }
 
 }
