85387e6d3e055dcead2ad64c0f8adf16399fb4bb
Iarlyson Santana <Iarlyson.Santana@gmail.com>
2021-04-11 19:42:19 -0300

    Pagina de editar pronta, alteraçoes no controler a ser revista

--- app/src/main/java/br/edu/ifpb/controllers/IntegranteController.java
+++ app/src/main/java/br/edu/ifpb/controllers/IntegranteController.java
@@ -1,13 +1,12 @@
 package br.edu.ifpb.controllers;
 
-import br.edu.ifpb.dao.BandaDao;
 import br.edu.ifpb.dao.IntegranteDao;
-import br.edu.ifpb.model.Banda;
 import br.edu.ifpb.model.Integrante;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.List;
 
 @Named
 @SessionScoped
@@ -17,5 +16,25 @@ public class IntegranteController implements Serializable {
 
     private IntegranteDao integranteDao = new IntegranteDao();
 
+    public void criarIntegrante(){
+        integranteDao.novo(integrante);
+    }
 
+    public void excluirIntegrante(){
+        integranteDao.excluir(integrante);
+    }
+
+    public List<Integrante> listarIntegrantes(){
+        return integranteDao.listar();
+    }
+    public void editarIntegrante(){
+        integranteDao.editar(integrante);
+    }
+    public Integrante getIntegrante() {
+        return integrante;
+    }
+
+    public void setIntegrante(Integrante integrante) {
+        this.integrante = integrante;
+    }
 }
