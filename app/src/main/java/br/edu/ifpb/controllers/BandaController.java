b95d882a6ab6547cb15cf947f249a9a193ea4ab8
daniel-dantas <daniel.dantas.ip19@hotmail.com>
2021-04-10 21:00:08 -0300

    Criando os controllers a ser usados pela aplicação

---
+++ app/src/main/java/br/edu/ifpb/controllers/BandaController.java
@@ -0,0 +1,10 @@
+package br.edu.ifpb.controllers;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@Named
+@SessionScoped
+public class BandaController implements Serializable {
+}
